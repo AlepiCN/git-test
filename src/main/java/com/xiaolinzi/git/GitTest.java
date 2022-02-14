199807f23e58058ec889730e0b9b954f347e9005
Alepi <351717461@qq.com>
2022-02-14 22:31:20 +0800

    git test commit

---
+++ src/main/java/com/xiaolinzi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xiaolinzi.git;
+
+/**
+ * @author Alepi
+ * version zero
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
